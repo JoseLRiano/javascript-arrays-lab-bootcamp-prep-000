e4e719e35a08254a44cf46dbeaff8c8d3e54f09b
Jose Riano <joselriano10@gmail.com>
2017-09-26 23:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function prependKitten(name){
   return [name,...kittens];
 }
 function removeLastKitten(){
-  kittens = kittens.slice(0,kittens.length-1);
-  return kittens;
+  return kittens.slice(0, kittens.length -1);
+
 }
